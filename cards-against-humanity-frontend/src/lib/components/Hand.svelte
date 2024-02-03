f961e7fabcdc108951652d996cfdef2edf539f6e
luaroncrew <ma-kir.ru@yandex.ru>
2024-02-03 13:23:29 +0100

    add player list

--- cards-against-humanity-frontend/src/lib/components/Hand.svelte
+++ cards-against-humanity-frontend/src/lib/components/Hand.svelte
@@ -1,24 +1,22 @@
 <script>
     import Card from '$lib/components/Card.svelte';
-    import { createEventDispatcher } from 'svelte';
-    const dispatch = createEventDispatcher();
 
-    export let cards = [];
+    export let playerCards = [];
     let selectedCard = null;
 
     function selectCard(card) {
         selectedCard = card;
-        dispatch('cardSelected', card);
     }
 </script>
 
 <div class="hand">
-    {#each cards as card}
-        <Card
-                {card}
-                isSelected={selectedCard === card}
-                on:click={() => selectCard(card)}
-        />
+    {#each playerCards as responseCard}
+        <div on:click="{()=> selectCard(responseCard)}">
+            <Card
+                    {responseCard}
+                    isSelected={selectedCard === responseCard}
+            />
+        </div>
     {/each}
 </div>
 
--- cards-against-humanity-frontend/src/routes/+page.svelte
+++ cards-against-humanity-frontend/src/routes/+page.svelte
@@ -1,11 +1,20 @@
 <script>
     import Hand from '$lib/components/Hand.svelte';
     import Prompt from '$lib/components/Prompt.svelte';
+    import PlayersList from '$lib/components/PlayersList.svelte';
+
     let promptCard = { text: "Server-chosen prompt card with a space _ ." };
     let playerCards = [
         { text: "Response Card 1" },
         { text: "Response Card 2" },
         // Add more cards
+    ];
+
+    let players = [
+        { name: "Player 1", isHighlighted: false },
+        { name: "Player 2", isHighlighted: true }, // Highlighted
+        { name: "Player 3", isHighlighted: false },
+        { name: "Player 4", isHighlighted: true }  // Highlighted
     ];
 
     function handleCardSelection(card) {
@@ -16,3 +25,4 @@
 
 <Prompt {promptCard} />
 <Hand {playerCards} on:cardSelected={handleCardSelection} />
+<PlayersList {players} />
