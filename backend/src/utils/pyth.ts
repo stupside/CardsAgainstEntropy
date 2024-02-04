93ef91f3c772275f41380eec795852428683dc45
luaroncrew <ma-kir.ru@yandex.ru>
2024-02-04 23:22:41 +0100

    add pyth wip

--- backend/src/utils/pyth.ts
+++ backend/src/utils/pyth.ts
@@ -1,5 +1,13 @@
+import {Web3} from "web3";
+
 export const getRandomNumber = (): number => {
   // TODO: generate a random seed with pyth
+  const web3 = new Web3();
+  const randomNumber = web3.utils.randomHex(32);
+  const commitment = web3.utils.keccak256(randomNumber);
+  console.log(commitment);
 
   return Math.random();
 };
+
+getRandomNumber()
